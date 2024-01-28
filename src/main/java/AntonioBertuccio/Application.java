d42c25c1761a9d403b692a1ba7840d275530c1ef
Antonio Bertuccio <anto.bertu@gmail.com>
2024-01-28 15:05:43 +0100

    fixed User.java with getter, setter, constructor and toString

--- src/main/java/AntonioBertuccio/Application.java
+++ src/main/java/AntonioBertuccio/Application.java
@@ -9,6 +9,8 @@ import AntonioBertuccio.dao.*;
 import AntonioBertuccio.entities.*;
 import AntonioBertuccio.enums.Genre;
 
+import java.time.LocalDate;
+
 public class Application {
   public static void main(String[] args) {
     System.out.println("Creazione dell'Entity Manager Factory (emf) per l'accesso al database.");
@@ -21,23 +23,25 @@ public class Application {
     UserDAO userDAO = new UserDAO(emf);
     LoanDAO loanDAO = new LoanDAO(emf);
 
+    User user = new User("Chiara", "Puleio", LocalDate.of(1994, 10, 29));
+
     // 📚 Aggiunta nuovi elementi
-    Book book = new Book();
-    book.setIsbn("1234567890123");
-    book.setYear(2022);
-    book.setPages(200);
-    book.setAuthor("John Doe");
-    book.setGenre(Genre.FICTION);
-    bookDAO.addBook(book);
+//    Book book = new Book();
+//    book.setIsbn("1234567890123");
+//    book.setYear(2022);
+//    book.setPages(200);
+//    book.setAuthor("John Doe");
+//    book.setGenre(Genre.FICTION);
+//    bookDAO.addBook(book);
 
     // 🔎 Ricerca libri per ISBN
-    String isbnToSearch = "1234567890123";
-    Book foundBook = bookDAO.findBookByIsbn(isbnToSearch);
-    if (foundBook != null) {
-      System.out.println("Libro trovato: " + foundBook.getTitle());
-    } else {
-      System.out.println("Nessun libro trovato con ISBN: " + isbnToSearch);
-    }
+//    String isbnToSearch = "1234567890123";
+//    Book foundBook = bookDAO.findBookByIsbn(isbnToSearch);
+//    if (foundBook != null) {
+//      System.out.println("Libro trovato: " + foundBook.getTitle());
+//    } else {
+//      System.out.println("Nessun libro trovato con ISBN: " + isbnToSearch);
+//    }
 
     System.out.println("Chiusura dell EntityManagerFactory alla fine dell'applicazione");
     emf.close();
--- src/main/java/AntonioBertuccio/entities/User.java
+++ src/main/java/AntonioBertuccio/entities/User.java
@@ -24,4 +24,52 @@ public class User {
   public User() {
   }
 
+  public User(String firstName, String lastName, LocalDate dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public LocalDate getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public void setDateOfBirth(LocalDate dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public List<Loan> getLoans() {
+    return loans;
+  }
+
+  public void setLoans(List<Loan> loans) {
+    this.loans = loans;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+            "id=" + id +
+            ", firstName='" + firstName + '\'' +
+            ", lastName='" + lastName + '\'' +
+            ", dateOfBirth=" + dateOfBirth +
+            ", loans=" + loans +
+            '}';
+  }
 }
